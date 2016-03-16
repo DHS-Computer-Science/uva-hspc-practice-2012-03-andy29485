b3a8ffb1b066cc85b78f0526e9234388f01ed24d
Noah <AndriyZasypkin@gmail.com>
2016-03-15 22:54:14 -0400

    If this works, I will not be happy

--- Main.java
+++ Main.java
@@ -2,72 +2,33 @@
 //2016-03-15
 //Practice 2014 - 06: Spawn of Ungoliant
 
-import java.io.*;
-
-class Rect {
-  private double width;
-  private double height;
-
-  public Rect(double x1, double y1, double x2, double y2) {
-    this.width  = x2-x1;
-    this.height = y2-y1;
-  }
-
-  public static Rect gen(double... points) {
-    double minx = Double.MAX_VALUE;
-    double miny = Double.MAX_VALUE;
-    double maxx = Double.MIN_VALUE;
-    double maxy = Double.MIN_VALUE;
-
-    for(int i=0; i<points.length; i++) {
-      if(i%2==0) {
-        if(points[i] < minx)
-          minx = points[i];
-        if(points[i] > maxx)
-          maxx = points[i];
-      }
-      else {
-        if(points[i] < miny)
-          miny = points[i];
-        if(points[i] > maxy)
-          maxy = points[i];
-      }
-    }
-
-    return new Rect(minx, miny, maxx, maxy);
-  }
-
-  public double perimeter() {
-    return width*2 + height*2;
-  }
-
-  public double area() {
-    return width*height;
-  }
-}
+import java.util.*;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    //create an input reader object
-    BufferedReader input
-            = new BufferedReader(new InputStreamReader(System.in));
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-    int nCases = Integer.valueOf(input.readLine());
+    int nCases = input.nextInt();
     for(int i=1; i<=nCases; i++) {
-      int   nAnts      = Integer.valueOf(input.readLine());
-      double[] points  = new double[nAnts*2];
-      for(int j=0; j<nAnts; j++) {
-        String[] point = input.readLine().split("\\s+");
-        points[2*j]    = Double.valueOf(point[0]);
-        points[2*j+1]  = Double.valueOf(point[1]);
-      }
-      Rect rect = Rect.gen(points);
-      System.out.println("Case "+i+": Area "+rect.area()+
-                         ", Perimeter "+rect.perimeter());
-    }
+      int    nAnts = input.nextInt();
+      double dMinX = Double.MAX_VALUE;
+      double dMinY = Double.MAX_VALUE;
+      double dMaxX = -Double.MAX_VALUE;
+      double dMaxY = -Double.MAX_VALUE;
 
-    //close stream
-    input.close();
+      for(int j=0; j<nAnts; j++) {
+        double tmpX = input.nextDouble();
+        double tmpY = input.nextDouble();
+
+        dMinX = Math.min(tmpX, dMinX);
+        dMinY = Math.min(tmpY, dMinY);
+        dMaxX = Math.max(tmpX, dMaxX);
+        dMaxY = Math.max(tmpY, dMaxY);
+      }
+      double a =   (dMaxX-dMinX) *   (dMaxY-dMinY);
+      double p = 2*(dMaxX-dMinX) + 2*(dMaxY-dMinY);
+      System.out.println("Case "+i+": Area "+a+", Perimeter "+p);
+    }
 
     System.exit(0);
   }
